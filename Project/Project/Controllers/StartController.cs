9970b2cdbecf11b50231b9a5cd5bf21cbdb6ab67
Alishba Mahmood <bcsf19a029@pucit.edu.pk>
2023-01-30 13:28:24 +0500

    New changes

--- Project/Project/Controllers/StartController.cs
+++ Project/Project/Controllers/StartController.cs
@@ -12,10 +12,31 @@ namespace Project.Controllers
         {
             return View();
         }
-
-        public IActionResult signUp()
+        
+        public IActionResult signup()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult signUpHandle(string Name, string username, string password, string Cpassword, string email, string phnNo)
+        {
+            Models.DB_function db = new Models.DB_function();
+            db.addStudent(Name,username,password,Cpassword,email,phnNo);
+            return Redirect("/start/signup");
+        }
+        [HttpPost]
+        public IActionResult loginHandle(string username,string password)
+        {
+            ViewBag.Name = username;
+            if (HttpContext.Request.Cookies.ContainsKey("username"))
+            {
+                ViewBag.Name= HttpContext.Request.Cookies["username"];
+            }
+            else
+            {
+                HttpContext.Response.Cookies.Append("username", username.ToString());
+            }
+            return Redirect("/pages/feed");
         }
 
 
--- Project/Project/Models/DB_function.cs
+++ Project/Project/Models/DB_function.cs
@@ -7,6 +7,21 @@ namespace Project.Models
 {
     public class DB_function
     {
+
+        public void addStudent(string Name, string username, string password, string Cpassword, string email, string phnNo)
+        {
+            Student s = new Student();
+            s.Id = 0;
+            s.Name = Name;
+            s.Username = username;
+            s.Password = password;
+            s.Email = email;
+            s.PhoneNo = phnNo;
+
+            ScoreifyContext storeStudent = new ScoreifyContext();
+            storeStudent.Student.Add(s);
+            storeStudent.SaveChanges();
+        }
         
     }
 }
